77e11121e05cf17918dc8cb8f1f176e6466aaf7a
Evelynn <voooidwaaalker@gmail.com>
2022-12-06 00:18:05 +0300

    init commit and done 1 homework

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿// 1 task
+// Console.WriteLine ("Enter a");
+// int a = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine ("Enter b");
+// int b = Convert.ToInt32 (Console.ReadLine());
+
+// if (a>b)
+// {
+//     Console.WriteLine($"max={a}");
+// }
+// else 
+// {
+//     Console.WriteLine($"max={b}");
+// }
+
+//2 task
+// Console.WriteLine("Enter a");
+// int a = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("Enter b");
+// int b = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("Enter c");
+// int c = Convert.ToInt32 (Console.ReadLine());
+// int max=a;
+// if (a>b)
+// {
+//     max = a;
+// } 
+// else 
+// {
+//     max = b;
+// };
+// if (max>c)
+// {
+//     Console.WriteLine ($"max = {max}");
+// }
+// else 
+// {
+//     Console.WriteLine ($"max = {c}");
+// }
+
+// //3 task
+// Console.WriteLine ("Enter number");
+// int a = Convert.ToInt32 (Console.ReadLine());
+// if (a % 2 == 0) 
+// {
+//     Console.WriteLine ("Число является четным");
+// }
+// else 
+// {
+//     Console.WriteLine ("Число является нечетным");
+// }
+
+// 4 Task
+// Console.WriteLine("Enter N");
+// int N = Convert.ToInt32 (Console.ReadLine());
+// for (int counter = 1; counter <= N; counter++)
+// {
+// if (counter % 2 == 0)
+// {
+// Console.Write(counter+" ");
+// }
+// } 
+
+// 2 вариант решения
+// Console.WriteLine("Enter N");
+// int N = Convert.ToInt32 (Console.ReadLine());
+// for (int counter = 1; counter <= N/2; counter++)
+// {
+// Console.Write(2*counter+" ");
+// }
